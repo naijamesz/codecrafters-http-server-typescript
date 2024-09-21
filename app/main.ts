0e43b4493da48d67142201eed9e1748873234724
naijamesz <kodlikejamesz@hotmail.com>
2024-09-21 10:27:55 +0700

    Tests passed

--- app/main.ts
+++ app/main.ts
@@ -1,13 +1,14 @@
-import * as net from "net";
+import * as net from 'net';
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+console.log('Logs from your program will appear here!');
 
 // Uncomment this to pass the first stage
-// const server = net.createServer((socket) => {
-//   socket.on("close", () => {
-//     socket.end();
-//   });
-// });
-//
-// server.listen(4221, "localhost");
+const server = net.createServer(socket => {
+  socket.on('close', () => {
+    socket.end();
+  });
+});
+
+server.listen(4221, 'localhost');
+
