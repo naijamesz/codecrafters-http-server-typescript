71c2051244d9c5013117181f65494ba44a055aa4
naijamesz <kodlikejamesz@hotmail.com>
2024-09-21 11:56:06 +0700

    HTTP respose200

--- app/main.ts
+++ app/main.ts
@@ -1,14 +1,37 @@
 import * as net from 'net';
 
+// Uncomment this to pass the first stage
+const server = net.createServer(socket => {
+  socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
+
+  // socket.on('close', () => {
+  socket.end();
+  // });
+});
+
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log('Logs from your program will appear here!');
 
-// Uncomment this to pass the first stage
-const server = net.createServer(socket => {
-  socket.on('close', () => {
-    socket.end();
-  });
+server.listen(4221, 'localhost', () => {
+  console.log(`Server is running on port : 4221`);
 });
 
-server.listen(4221, 'localhost');
+/*
+>Note:การตอบสนอง HTTP ประกอบด้วยสามส่วน โดยแต่ละส่วนคั่นด้วยCRLF ( \r\n):
+เส้นสถานะ
+ส่วนหัวตั้งแต่ศูนย์รายการขึ้นไป โดยแต่ละรายการลงท้ายด้วย CRLF
+เนื้อหาตอบกลับที่เป็นตัวเลือก
+
+*Status line
+HTTP/1.1  // HTTP version
+200       // Status code
+OK        // Optional reason phrase
+\r\n      // CRLF that marks the end of the status line
+
+*Headers (empty)
+\r\n      // CRLF that marks the end of the headers
+
+Response body (empty)
+
+*/
 
